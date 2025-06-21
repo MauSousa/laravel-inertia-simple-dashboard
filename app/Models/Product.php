d66ccaca0cde95db3894949448aba693dad46b07
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-20 19:07:58 -0600

    Add fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,4 +11,11 @@ class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+    ];
 }
