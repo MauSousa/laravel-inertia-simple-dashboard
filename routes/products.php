e257ac48d0d246dfe7e0eda5e6363a52451a1d6d
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-21 14:13:21 -0600

    Add create product route

--- routes/products.php
+++ routes/products.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('products')->name('products.')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('index');
+    Route::get('/create', [ProductController::class, 'create'])->name('create');
 });
