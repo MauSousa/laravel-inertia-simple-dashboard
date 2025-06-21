7a52388390bb2c86cf8ac4141957ab4bf147b3ab
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-20 19:07:23 -0600

    Add to array product test

--- tests/Feature/Product/ProductTest.php
+++ tests/Feature/Product/ProductTest.php
@@ -19,3 +19,18 @@ test('users not authenticated can not see products page', function () {
     $response = $this->get('/products');
     $response->assertRedirect('/login');
 });
+
+test('to array', function () {
+    $product = Product::factory()->create()->refresh();
+
+    expect(array_keys($product->toArray()))
+        ->toBe([
+            'id',
+            'name',
+            'description',
+            'price',
+            'image',
+            'created_at',
+            'updated_at',
+        ]);
+});
