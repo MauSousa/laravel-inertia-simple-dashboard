cdf354baf5c7274b1d78e908064010cc9f63f323
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-20 18:53:20 -0600

    Add products routes file

---
+++ routes/products.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'verified'])->prefix('products')->name('products.')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('index');
+});
