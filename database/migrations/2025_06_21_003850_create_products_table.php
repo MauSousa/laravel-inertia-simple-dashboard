35919fe5ff04b79036245506510e8b825d42dcf1
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-20 19:07:48 -0600

    Add products fields

--- database/migrations/2025_06_21_003850_create_products_table.php
+++ database/migrations/2025_06_21_003850_create_products_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->string('image');
             $table->timestamps();
         });
     }
