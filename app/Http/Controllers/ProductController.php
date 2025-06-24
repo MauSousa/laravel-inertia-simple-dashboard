6c5fc05953ef9d515fc8f0f4ce9811963bf42777
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-24 15:46:17 -0600

    Apply linting rules

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,40 +4,51 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Actions\Products\CreateProduct;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Response;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        return inertia('Products/Index', []);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return inertia('Products/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreProductRequest $request)
+    public function store(StoreProductRequest $request, CreateProduct $action): RedirectResponse
     {
-        //
+        $validated = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $validated['image'] = $action->uploadFile($request, '/');
+        }
+
+        $action->handle($request->validated());
+
+        return to_route('products.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show(Product $product): void
     {
         //
     }
@@ -45,7 +56,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(Product $product): void
     {
         //
     }
@@ -53,7 +64,7 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(UpdateProductRequest $request, Product $product): void
     {
         //
     }
@@ -61,7 +72,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(Product $product): void
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
-require __DIR__.'/products.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/products.php';
