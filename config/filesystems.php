51e667b18c0fdb177720fda2be951cfd6d4b7260
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-24 15:31:15 -0600

    Add products disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,7 +43,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
+
+        'products' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/products'),
+            'url' => env('APP_URL') . '/storage/products',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
