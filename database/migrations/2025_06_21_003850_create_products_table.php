6f9cdf59ea2594091be607c93d018fff0540d5d3
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-24 15:26:27 -0600

    Add nullable for description and image fields

--- database/migrations/2025_06_21_003850_create_products_table.php
+++ database/migrations/2025_06_21_003850_create_products_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
