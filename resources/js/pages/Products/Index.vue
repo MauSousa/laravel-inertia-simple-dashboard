180f17c0a754c4d5d2e09de5b48818f35ec2fb6e
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-30 15:06:18 -0600

    Add simple layout to show products

--- resources/js/pages/Products/Index.vue
+++ resources/js/pages/Products/Index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import TextLink from '@/components/TextLink.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
+import { Product, type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -10,6 +11,12 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: route('products.index'),
     },
 ];
+
+interface Props {
+    products: Product[];
+}
+
+const props = defineProps<Props>();
 </script>
 
 <template>
@@ -29,7 +36,20 @@ const breadcrumbs: BreadcrumbItem[] = [
                 </div>
             </div>
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
+                <TextLink :href="route('products.create')" class="underline underline-offset-4" :tabindex="6">Create Product</TextLink>
+                <div class="mt-4 grid auto-rows-min gap-4 md:grid-cols-3">
+                    <!--{{ props.products }}-->
+                    <div
+                        v-for="product in props.products"
+                        :key="product.id"
+                        class="flex flex-col gap-3 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                    >
+                        <p>Name: {{ product.name }}</p>
+                        <p>Description: {{ product.description }}</p>
+                        <p>Price: {{ product.price }}</p>
+                        <img :src="product.image" loading="lazy" alt="Product image" class="h-72 w-72 object-contain" />
+                    </div>
+                </div>
             </div>
         </div>
     </AppLayout>
