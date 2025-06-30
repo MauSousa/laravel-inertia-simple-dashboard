57a600830b0a8ccf02fe686dc4350800aa6750a1
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-30 15:00:17 -0600

    Change file validation

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -29,7 +29,7 @@ class StoreProductRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'description' => ['nullable', 'string', 'max:255'],
             'price' => ['required', 'numeric', 'min:0'],
-            'image' => ['nullable', File::types(['png', 'jpg', 'jpeg'])->max(5 * 1024)],
+            'image' => ['nullable', File::types(['png', 'jpg', 'jpeg'])->min('1kb')->max('5mb')],
         ];
     }
 }
