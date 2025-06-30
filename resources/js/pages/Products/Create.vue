bd87683fdb3ae4db4d0b916c45941a5ae3fd7d1a
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-06-30 15:05:39 -0600

    Add form to create product view

--- resources/js/pages/Products/Create.vue
+++ resources/js/pages/Products/Create.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
-import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import InputError from '@/components/InputError.vue';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
+import { LoaderCircle } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
+        title: 'Products',
+        href: '/products',
+    },
+    {
         title: 'Create Product',
-        href: route('products.create'),
+        href: '/products/create',
     },
 ];
+
+const form = useForm({
+    name: '',
+    description: '',
+    price: 0,
+    image: null,
+});
+
+const submit = () => {
+    form.post(route('products.store'), {
+        onSuccess: () => form.reset('name', 'description', 'image'),
+    });
+};
 </script>
 
 <template>
@@ -17,19 +38,35 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-            </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
+            <div class="relative min-h-[100vh] flex-1 rounded-xl">
+                <form @submit.prevent="submit" class="flex flex-col gap-6">
+                    <div class="grid gap-6">
+                        <div class="grid gap-2">
+                            <Label for="name"> Name of Product </Label>
+                            <Input id="name" type="text" required autofocus placeholder="Name of Product" :tabindex="1" v-model="form.name" />
+                            <InputError :message="form.errors.name" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="price"> Price of Product </Label>
+                            <Input id="price" type="number" required autofocus placeholder="Price of Product" :tabindex="2" v-model="form.price" />
+                            <InputError :message="form.errors.price" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="description"> Description </Label>
+                            <Input id="description" type="text" placeholder="Description" :tabindex="2" v-model="form.description" />
+                            <InputError :message="form.errors.description" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="image"> Image of the product </Label>
+                            <Input id="image" type="file" @input="form.image = $event.target.files[0]" :tabindex="3" />
+                            <InputError :message="form.errors.image" />
+                        </div>
+                        <Button type="submit" class="mt-2 w-full" tabindex="5" :disabled="form.processing">
+                            <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
+                            Create product
+                        </Button>
+                    </div>
+                </form>
             </div>
         </div>
     </AppLayout>
