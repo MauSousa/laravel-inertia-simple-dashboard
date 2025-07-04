dcce5a67f36db614d29c613fc964df85676da1e7
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-03 18:56:40 -0600

    Change inertia's useform for laravel precognition useform

--- resources/js/pages/Products/Create.vue
+++ resources/js/pages/Products/Create.vue
@@ -5,7 +5,8 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head, useForm } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
+import { useForm } from 'laravel-precognition-vue-inertia';
 import { LoaderCircle } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -19,16 +20,19 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const form = useForm({
+const form = useForm('post', route('products.store'), {
     name: '',
     description: '',
     price: 0,
     image: null,
 });
 
+form.setValidationTimeout(500);
+form.validateFiles();
+
 const submit = () => {
     form.post(route('products.store'), {
-        onSuccess: () => form.reset('name', 'description', 'image'),
+        onSuccess: () => form.reset('name', 'description', 'price', 'image'),
     });
 };
 </script>
@@ -43,22 +47,53 @@ const submit = () => {
                     <div class="grid gap-6">
                         <div class="grid gap-2">
                             <Label for="name"> Name of Product </Label>
-                            <Input id="name" type="text" required autofocus placeholder="Name of Product" :tabindex="1" v-model="form.name" />
+                            <Input
+                                id="name"
+                                type="text"
+                                required
+                                autofocus
+                                placeholder="Name of Product"
+                                :tabindex="1"
+                                v-model="form.name"
+                                @change="form.validate('name')"
+                            />
                             <InputError :message="form.errors.name" />
                         </div>
                         <div class="grid gap-2">
                             <Label for="price"> Price of Product </Label>
-                            <Input id="price" type="number" required autofocus placeholder="Price of Product" :tabindex="2" v-model="form.price" />
+                            <Input
+                                id="price"
+                                type="number"
+                                required
+                                autofocus
+                                placeholder="Price of Product"
+                                :tabindex="2"
+                                v-model="form.price"
+                                @change="form.validate('price')"
+                            />
                             <InputError :message="form.errors.price" />
                         </div>
                         <div class="grid gap-2">
                             <Label for="description"> Description </Label>
-                            <Input id="description" type="text" placeholder="Description" :tabindex="2" v-model="form.description" />
+                            <Input
+                                id="description"
+                                type="text"
+                                placeholder="Description"
+                                :tabindex="2"
+                                v-model="form.description"
+                                @change="form.validate('description')"
+                            />
                             <InputError :message="form.errors.description" />
                         </div>
                         <div class="grid gap-2">
                             <Label for="image"> Image of the product </Label>
-                            <Input id="image" type="file" @input="form.image = $event.target.files[0]" :tabindex="3" />
+                            <Input
+                                id="image"
+                                type="file"
+                                @input="form.image = $event.target.files[0]"
+                                @change="form.validate('image')"
+                                :tabindex="3"
+                            />
                             <InputError :message="form.errors.image" />
                         </div>
                         <Button type="submit" class="mt-2 w-full" tabindex="5" :disabled="form.processing">
