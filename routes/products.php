a05e2db3f64d0ff77761e675c612d35f60282455
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-03 18:53:50 -0600

    Add laravel precognition middleware

--- routes/products.php
+++ routes/products.php
@@ -3,12 +3,13 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\ProductController;
+use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('products')->name('products.')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('index');
     Route::get('/create', [ProductController::class, 'create'])->name('create');
-    Route::post('/', [ProductController::class, 'store'])->name('store');
+    Route::post('/', [ProductController::class, 'store'])->middleware([HandlePrecognitiveRequests::class])->name('store');
     Route::get('/{product}', [ProductController::class, 'show'])->name('show');
     Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('edit');
 });
