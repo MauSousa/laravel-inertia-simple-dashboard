3e983ed56cd2ac01f19099a2f82bda8e4422d7ab
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-07 19:11:02 -0600

    Add update product action

---
+++ app/Actions/Products/UpdateProduct.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Products;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
+
+class UpdateProduct
+{
+    /**
+     * Handle the action.
+     *
+     * @param  array<string, mixed>  $data
+     */
+    public function handle(Product $product, array $data): void
+    {
+        $product->update([
+            'name' => $data['name'] ?? $product->name,
+            'description' => $data['description'] ?? $product->description,
+            'price' => $data['price'] ?? $product->price,
+            'image' => $data['image'] ?? $product->image,
+        ]);
+
+        $product->refresh();
+    }
+
+    /**
+     * Upload file to the given path.
+     */
+    public function uploadFile(Request $request): string
+    {
+        $upload = $request->file('image');
+        $filename = Str::uuid() . '.' . $upload->extension();
+        $image = Image::read($upload)
+            ->resize(300, 200);
+
+        Storage::disk('products')->put(
+            $filename,
+            $image->encodeByExtension($upload->extension()) // @phpstan-ignore-line
+        );
+
+        return $filename;
+    }
+}
