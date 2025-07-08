6fb21476206076447d01b310228a29ebeda3a0bd
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-08 17:27:44 -0600

    Change validation rule

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -26,7 +26,7 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255', Rule::unique('products')->ignore($this->product->id)],
+            'name' => ['required', 'string', 'max:255', Rule::unique('products')->ignore($this->product)],
             'description' => ['nullable', 'string', 'max:255'],
             'price' => ['required', 'numeric', 'min:0'],
             'image' => ['nullable', File::types(['png', 'jpg', 'jpeg'])->min('1kb')->max('5mb')],
