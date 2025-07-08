1f5f06b9e436c55f1f9c1661013b7e8bbb353185
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-08 17:36:40 -0600

    Add edit view form

--- resources/js/pages/Products/Edit.vue
+++ resources/js/pages/Products/Edit.vue
@@ -1,15 +1,58 @@
 <script setup lang="ts">
-import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import InputError from '@/components/InputError.vue';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Product, type BreadcrumbItem } from '@/types';
+import { Head, router } from '@inertiajs/vue3';
+import { useForm } from 'laravel-precognition-vue-inertia';
+import { LoaderCircle } from 'lucide-vue-next';
+
+interface Props {
+    product: Product;
+}
+
+const props = defineProps<Props>();
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Edit Product',
-        href: route('products.edit'),
+        title: 'Products',
+        href: route('products.index'),
+    },
+    {
+        title: `Show ${props.product.name}`,
+        href: route('products.show', props.product.id),
+    },
+    {
+        title: `Edit ${props.product.name}`,
+        href: '',
     },
 ];
+
+const form = useForm('patch', route('products.update', props.product), {
+    name: props.product.name,
+    description: props.product.description,
+    price: props.product.price,
+    image: null,
+});
+
+form.setValidationTimeout(500);
+form.validateFiles();
+
+const submit = () => {
+    router.post(
+        route('products.update', props.product),
+        {
+            _method: 'patch',
+            ...form.data(),
+        },
+        {
+            preserveScroll: true,
+            onSuccess: () => form.reset('name', 'description', 'price', 'image'),
+        },
+    );
+};
 </script>
 
 <template>
@@ -17,19 +60,67 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-            </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
+            <div class="grid min-h-[100vh] grid-cols-4 gap-2 rounded-xl">
+                <form @submit.prevent="submit" class="col-span-3 flex flex-col gap-6">
+                    <div class="grid gap-6">
+                        <div class="grid gap-2">
+                            <Label for="name"> Name of Product </Label>
+                            <Input
+                                id="name"
+                                type="text"
+                                required
+                                autofocus
+                                placeholder="Name of Product"
+                                :tabindex="1"
+                                v-model="form.name"
+                                @change="form.validate('name')"
+                            />
+                            <InputError :message="form.errors.name" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="price"> Price of Product </Label>
+                            <Input
+                                id="price"
+                                type="number"
+                                required
+                                autofocus
+                                placeholder="Price of Product"
+                                :tabindex="2"
+                                v-model="form.price"
+                                @change="form.validate('price')"
+                            />
+                            <InputError :message="form.errors.price" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="description"> Description </Label>
+                            <Input
+                                id="description"
+                                type="text"
+                                placeholder="Description"
+                                :tabindex="3"
+                                v-model="form.description"
+                                @change="form.validate('description')"
+                            />
+                            <InputError :message="form.errors.description" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="image"> Image of the product </Label>
+                            <Input
+                                id="image"
+                                type="file"
+                                @input="form.image = $event.target.files[0]"
+                                @change="form.validate('image')"
+                                :tabindex="4"
+                            />
+                            <InputError :message="form.errors.image" />
+                        </div>
+                        <Button type="submit" class="mt-2 w-full" tabindex="5" :disabled="form.processing">
+                            <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
+                            Update product
+                        </Button>
+                    </div>
+                </form>
+                <div class="col-span-1"><img :src="`/storage/products/${props.product.image}`" class="w-full rounded-xl" /></div>
             </div>
         </div>
     </AppLayout>
