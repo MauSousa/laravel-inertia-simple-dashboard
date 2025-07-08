dbedf3dea8adef7835dc0500bcf26eec26474041
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-07 19:29:39 -0600

    Add delete product action

---
+++ app/Actions/Products/DeleteProduct.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Products;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteProduct
+{
+    public function handle(Product $product): void
+    {
+        Storage::disk('products')->delete($product->image);
+
+        $product->delete();
+    }
+}
