bae0c3d8fb151ff2b0b13abe87fa7b53f5b74289
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-25 17:53:30 -0600

    Add interface for user model

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -43,4 +43,13 @@ export interface Product {
     image: string;
 }
 
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    email_verified_at: string | null;
+    created_at: string;
+    updated_at: string;
+}
+
 export type BreadcrumbItemType = BreadcrumbItem;
