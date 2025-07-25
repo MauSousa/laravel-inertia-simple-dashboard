61928c7cac223698c7f96baa1da9587037cde05b
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-25 14:08:40 -0600

    Add user request

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // TODO: Manage authorization with policies
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', Rule::email()->rfcCompliant(true)->validateMxRecord()->preventSpoofing(), 'max:100', 'unique:users'],
+        ];
+    }
+}
