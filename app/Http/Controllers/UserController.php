6b6a326b06a4742a6bf17b5067dde2dbd7f6871d
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-07-25 14:07:53 -0600

    Add user controller to handle user dashboard actions

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Actions\Users\CreateUser;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Users/Index', [
+            'users' => User::all()->map(fn ($user): array => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'email_verified_at' => $user->email_verified_at ? Carbon::parse($user->email_verified_at)->format('H:i:s d-M-Y') : 'Email not verified',
+                'created_at' => Carbon::parse($user->created_at)->format('H:i:s d-M-Y'),
+                'updated_at' => Carbon::parse($user->updated_at)->format('H:i:s d-M-Y'),
+            ]),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Users/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request, CreateUser $action): RedirectResponse
+    {
+        $action->handle($request->validated());
+
+        return to_route('users.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user): void
+    {
+        //
+    }
+}
